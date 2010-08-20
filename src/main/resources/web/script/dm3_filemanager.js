79d3d4ee033058f1082192b6655ee2c2e18c40e5
Jörg Richter <jri@deepamehta.de>
2010-08-20 18:18:22 +0200

    Makes use of Canvas's new grid positioning feature to position dropped files and folders.

--- src/main/resources/web/script/dm3_filemanager.js
+++ src/main/resources/web/script/dm3_filemanager.js
@@ -1,19 +1,17 @@
 function dm3_filemanager() {
 
-
-
-    /**************************************************************************************************/
-    /**************************************** Overriding Hooks ****************************************/
-    /**************************************************************************************************/
-
-
+    // ------------------------------------------------------------------------------------------------ Overriding Hooks
 
     this.process_files_drop = function(files) {
+        canvas.start_grid_positioning()
+        //
         for (var i = 0; i < files.get_directory_count(); i++) {
             get_plugin("dm3_files").create_file_topics(files.get_directory(i))
         }
         for (var i = 0; i < files.get_file_count(); i++) {
             get_plugin("dm3_files").create_file_topic(files.get_file(i))
         }
+        //
+        canvas.stop_grid_positioning()
     }
 }
