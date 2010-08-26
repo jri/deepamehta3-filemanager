a4477c0a0372f95e9f4819406f3320d71a1779ad
Jörg Richter <jri@deepamehta.de>
2010-08-26 16:59:27 +0200

    Bug fix: when dropping just files (no folders) the first created topic is selected (instead of no selection).

--- src/main/resources/web/script/dm3_filemanager.js
+++ src/main/resources/web/script/dm3_filemanager.js
@@ -5,11 +5,12 @@ function dm3_filemanager() {
     this.process_files_drop = function(files) {
         canvas.start_grid_positioning()
         //
-        for (var i = 0; i < files.get_directory_count(); i++) {
+        var dir_count = files.get_directory_count()
+        for (var i = 0; i < dir_count; i++) {
             get_plugin("dm3_files").create_file_topics(files.get_directory(i), i == 0)
         }
         for (var i = 0; i < files.get_file_count(); i++) {
-            get_plugin("dm3_files").create_file_topic(files.get_file(i))
+            get_plugin("dm3_files").create_file_topic(files.get_file(i), !dir_count && i == 0)
         }
         //
         canvas.stop_grid_positioning()
